4590c1f1740d1593d013583851b7da5c4f78a065
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-07-28 01:28:35 +0530

    Some bugs fixed
    Some bugs fixed

--- YouTube Video Downloader/YouTube.py
+++ YouTube Video Downloader/YouTube.py
@@ -1,5 +1,5 @@
 import pafy
-
+from tkinter.messagebox import *
 myvid = None
 
 def YouTubeLink(link,l1,l2,l3,l4):
@@ -13,21 +13,27 @@ def YouTubeLink(link,l1,l2,l3,l4):
 #===============360px Video Quality=====================
 def Low(path):
     global myvid
-    Video_Low_Quality=myvid.streams[0]
-    print("\nVideo Size:",(int(Video_Low_Quality.get_filesize())//1024)//1024,"MB")
-    print("Video Resolution:",Video_Low_Quality.resolution)
-    print("Video Extension:",Video_Low_Quality.extension)
-    print("\nDownloading Progress...\n")
-    Video_Low_Quality.download(filepath=path)  # downloading start at resuming
-    print("\nDownload Complete...\n")
+    try:
+        Video_Low_Quality=myvid.streams[0]
+        print("\nVideo Size:",(int(Video_Low_Quality.get_filesize())//1024)//1024,"MB")
+        print("Video Resolution:",Video_Low_Quality.resolution)
+        print("Video Extension:",Video_Low_Quality.extension)
+        print("\nDownloading Progress...\n")
+        Video_Low_Quality.download(filepath=path)  # downloading start at resuming
+        print("\nDownload Complete...\n")
+    except IndexError:
+        showerror("Error","This YouTube Video Not Support Low Quality")
 
 #===============720px Video Quality=====================
 def High(path):
     global myvid
-    Video_High_Quality=myvid.streams[1]
-    print("\nVideo Size:",(int(Video_High_Quality.get_filesize())//1024)//1024,"MB")
-    print("Video Resolution:",Video_High_Quality.resolution)
-    print("Video Extension:",Video_High_Quality.extension)
-    print("\nDownloading Progress...\n")
-    Video_High_Quality.download(filepath=path)  # downloading start at resuming
-    print("\nDownload Complete...\n")
+    try:
+        Video_High_Quality=myvid.streams[1]
+        print("\nVideo Size:",(int(Video_High_Quality.get_filesize())//1024)//1024,"MB")
+        print("Video Resolution:",Video_High_Quality.resolution)
+        print("Video Extension:",Video_High_Quality.extension)
+        print("\nDownloading Progress...\n")
+        Video_High_Quality.download(filepath=path)  # downloading start at resuming
+        print("\nDownload Complete...\n")
+    except IndexError:
+        showerror("Error","This YouTube Video Not Support High Quality")
