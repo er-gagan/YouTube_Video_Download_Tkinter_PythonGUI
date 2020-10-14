8db4c51832a4d0e083e851f705134acd0106bcc6
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-10-14 19:43:08 +0530

    Adding title of video functionality
    Adding title of video functionality

--- YouTube Video Downloader/YouTube.py
+++ YouTube Video Downloader/YouTube.py
@@ -15,7 +15,8 @@ def Low(path):
     global myvid
     try:
         Video_Low_Quality=myvid.streams[0]
-        print("\nVideo Size:",(int(Video_Low_Quality.get_filesize())//1024)//1024,"MB")
+        print("\nTitle:",Video_Low_Quality.title)
+        print("Video Size:",(int(Video_Low_Quality.get_filesize())//1024)//1024,"MB")
         print("Video Resolution:",Video_Low_Quality.resolution)
         print("Video Extension:",Video_Low_Quality.extension)
         print("\nDownloading Progress...\n")
@@ -29,7 +30,8 @@ def High(path):
     global myvid
     try:
         Video_High_Quality=myvid.streams[1]
-        print("\nVideo Size:",(int(Video_High_Quality.get_filesize())//1024)//1024,"MB")
+        print("\nTitle:",Video_High_Quality.title)
+        print("Video Size:",(int(Video_High_Quality.get_filesize())//1024)//1024,"MB")
         print("Video Resolution:",Video_High_Quality.resolution)
         print("Video Extension:",Video_High_Quality.extension)
         print("\nDownloading Progress...\n")
